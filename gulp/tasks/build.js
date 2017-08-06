75b87a8a6227c552b1b6d6cee0d66e13545850d1
Adrian Kuma <adrian@devanddeliver.com>
2017-08-06 19:46:21 +0200

    Removed gulp server on server build

--- gulp/tasks/build.js
+++ gulp/tasks/build.js
@@ -1,8 +1,9 @@
 const
     gulp            = require('gulp'),
-    gulpSequence    = require('gulp-sequence')
+    gulpSequence    = require('gulp-sequence'),
+    { ENV_DEV }     = require('../envs')
 
 
-gulp.task('pre-build', ['clean', 'server', 'watch'])
+gulp.task('pre-build', ENV_DEV ? ['clean', 'server', 'watch'] : ['clean'])
 
 gulp.task('build', gulpSequence('pre-build', ['static', 'liquid', 'scripts', 'styles', 'examples']))
